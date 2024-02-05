d078e162c2858ba35833239ad594ce3297a3b029
zhengdao <316321454@qq.com>
2024-02-05 17:23:28 +0100

    visitor

--- src/ast/interfaces/IASTvisitor.java
+++ src/ast/interfaces/IASTvisitor.java
@@ -9,10 +9,14 @@ import ast.Dirs;
 import ast.Gather;
 import ast.Query;
 import ast.Rand;
+import ast.astQuery.BQuery;
+import ast.astQuery.GQuery;
 import fr.sorbonne_u.cps.sensor_network.requests.interfaces.ExecutionStateI;
 
 public interface IASTvisitor<Result, Data, Anomaly extends Throwable> {
 	Result visit(Query<?> ast, Data data) throws Anomaly;
+	Result visit(BQuery ast, Data data) throws Anomaly;
+	Result visit(GQuery ast, Data data) throws Anomaly;
     Result visit(Base ast, Data data) throws Anomaly;
     Result visit(Bexp ast, Data data) throws Anomaly;
     Result visit(Cexp ast, Data data) throws Anomaly;
@@ -21,7 +25,6 @@ public interface IASTvisitor<Result, Data, Anomaly extends Throwable> {
     Result visit(Dirs ast, Data data) throws Anomaly;
     Result visit(Gather ast, Data data) throws Anomaly;
     Result visit(Rand ast, Data data) throws Anomaly;
-//	Object visit(Query<?> ast, ExecutionStateI data) throws Exception;
 
 
 }
